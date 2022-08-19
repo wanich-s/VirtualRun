2a58a1aff557b8b509a991cd8f2cab7f98765319
Wanich <wanich.s008@gmail.com>
2022-08-19 14:07:49 +0700

    Add logged in time

--- api.php
+++ api.php
@@ -1,11 +1,28 @@
 <?php
     // only ajax request
-    if(!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
+    if(!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {        
+
+        if(isset($_SESSION['LOGGED_TIME'])) {
+            // session started more than 30 minutes ago
+            if (time() - $_SESSION['LOGGED_TIME'] < 1800) {
+                session_regenerate_id(true);    // change session ID for the current session and invalidate old session ID
+                $_SESSION['LOGGED_TIME'] = time();  // update creation time
+            }else {
+                echo json_encode(array("logged" => false));
+                exit(0);
+            }
+        }else {
+            echo json_encode(array("logged" => false));
+            exit(0);
+        }
+
         switch ($_REQUEST['func']) {
             case 'login':
-                include ('functions/login.php');
+                include 'functions/login.php';
                 break;
-            
+            case 'application':
+                include 'functions/application.php';
+                break;
             default:
                 # code...
                 break;
--- functions/login.php
+++ functions/login.php
@@ -1,9 +1,16 @@
 <?php
+    session_start();
     // require 'config.php';
 
     // $sql = "SELECT * FROM Users";
     // $query = $mysqli -> query($sql);
     // $result = $query -> fetch_all(MYSQLI_ASSOC);
+    $user_id = '1';
+    if(!isset($_SESSION['LoggedIn'])) {
+        $_SESSION[UserID] = $user_id;
+        session_write_close();
+    }
+    
     
     echo json_encode(array("logged" => true));
 ?>
