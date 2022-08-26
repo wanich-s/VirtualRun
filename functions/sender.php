e7b299e0570c6a9d64ee8d637f2168166d0cbe4a
csn advance <sisa@sisa.local>
2022-08-26 17:00:47 +0700

    add file

---
+++ functions/sender.php
@@ -0,0 +1,91 @@
+<?php
+
+    /*
+    CREATE TABLE `Sender` (
+        `id` int(11) NOT NULL,
+        `address` text NOT NULL,
+        `last_update` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+    ALTER TABLE `Sender` ADD PRIMARY KEY (`id`);
+
+    INSERT INTO `Sender` (`id`, `address`, `last_update`) VALUES (1, 'aaa', '2022-08-26 09:34:25');
+    */
+    require 'config.php';
+    switch ($_SERVER['REQUEST_METHOD']) {
+        case 'GET':
+            getSenderInfo();
+            break;
+        case 'POST':
+            doSender();
+            break;
+        case 'PUT':
+            UpdateSender();
+            break;
+        default:
+            # code...
+            break;
+    }
+
+    function getSenderInfo() {
+        global $mysqli;
+        $query = $mysqli -> query("SELECT * FROM Sender;");
+        $sender_info = $query -> fetch_array(MYSQLI_ASSOC);
+
+        $mysqli->close();
+        echo json_encode(array('logged' => true, 'sender_info' => $sender_info['address']));
+    }
+
+    function doSender() {
+        global $mysqli;        
+
+        $sql = "INSERT INTO Sender (address)
+            VALUES (?);";
+        $stmt = $mysqli -> prepare($sql);
+        $stmt -> bind_param('s',  $address); 
+
+        $username = htmlspecialchars($_REQUEST['username']);
+        $address = htmlspecialchars($_REQUEST['address']);
+       
+        $stmt -> execute();
+        $stmt -> close();
+        $user_query = $mysqli -> query('SELECT LAST_INSERT_ID() AS user_id;');
+        $user = $user_query -> fetch_array(MYSQLI_ASSOC);
+
+        // Disconnect
+        $mysqli->close();
+
+        if($user) {
+            logged($user['user_id'], $username);
+            exit(0);
+        }
+        echo json_encode(array("logged" => false,'sql' => $sql));
+    }
+
+    function UpdateSender(){
+        global $mysqli;        
+
+        $sql = "INSERT INTO Sender (address)
+            VALUES (?);";
+        $stmt = $mysqli -> prepare($sql);
+        $stmt -> bind_param('s',  $address); 
+
+        $username = htmlspecialchars($_REQUEST['username']);
+        $address = htmlspecialchars($_REQUEST['address']);
+       
+        $stmt -> execute();
+        $stmt -> close();
+        $user_query = $mysqli -> query('SELECT LAST_INSERT_ID() AS user_id;');
+        $user = $user_query -> fetch_array(MYSQLI_ASSOC);
+
+        // Disconnect
+        $mysqli->close();
+
+        if($user) {
+            logged($user['user_id'], $username);
+            exit(0);
+        }
+        echo json_encode(array("logged" => false,'sql' => $sql));
+    }
+
+?>
